103e444112f6ef67d22332b484af418d6ebbb748
Axekill <andrei3.ru@ya.ru>
2023-05-02 11:08:35 +0300

    first commit

--- src/main/java/com/example/homework/Employee.java
+++ src/main/java/com/example/homework/Employee.java
@@ -1,6 +1,10 @@
 package com.example.homework;
 
+import org.apache.commons.lang3.StringUtils;
+
 import java.util.Objects;
+
+import static org.apache.commons.lang3.StringUtils.capitalize;
 
 public class Employee {
 
@@ -10,8 +14,8 @@ public class Employee {
     private int departmentId;
 
     public Employee(String firstName, String lastName, int salary, int departmentId) {
-        this.firstName = firstName;
-        this.lastName = lastName;
+        this.firstName = capitalize(firstName.toLowerCase());
+        this.lastName = capitalize(lastName.toLowerCase());
         this.salary = salary;
         this.departmentId = departmentId;
     }
